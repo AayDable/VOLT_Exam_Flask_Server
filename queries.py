0d7c22b95b2e332138bcb3a3caa3848d13963083
Aayush Arun Dable <aayush.dable@hubblehox.com>
2026-01-19 17:31:19 +0530

    Changed query

--- queries.py
+++ queries.py
@@ -5,8 +5,19 @@ pgsql_queries = \
 
 """
 select c2."rollNo", c2."candidateName", eh."hallName", c."courseName", 
-        pm."examDate", t."TotalCandidateScore", t."MaxPossibleScore", 
-        t."ScorePercentage"
+        pm."examDate", 
+        CASE 
+          WHEN ec."candidateExamStartTime" is null THEN NULL
+          ELSE t."TotalCandidateScore"  
+        END as "TotalCandidateScore",
+        CASE 
+          WHEN ec."candidateExamStartTime" is null THEN NULL
+          ELSE t."MaxPossibleScore"  
+        END as "MaxPossibleScore",
+        CASE 
+          WHEN ec."candidateExamStartTime" is null THEN NULL
+          ELSE t."ScorePercentage"  
+        END as "ScorePercentage"
 from (
     select rs."projectMasterId", rs."candidateId", 
             sum(rs."score") as "TotalCandidateScore", 
@@ -20,6 +31,7 @@ left outer join "projectMaster" pm on pm.id = t."projectMasterId"
 left outer join "course" c on c.id = pm."courseId"
 left outer join "candidate" c2 on c2.id = t."candidateId"
 left outer join "examHall" eh on eh.id = c2."examHallId"
+left outer join "examConfig" ec on ec."candidateId" = c2."id"
 where pm."isGeneratingScores" = false
 and pm."isDeleted" = false
 and pm."enabledBy" > 0
